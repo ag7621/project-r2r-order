a2c60325e0a43ade936721080daef84ef0bc2a5b
ag7621 <aaron.c.guerra@gmail.com>
2025-03-13 20:56:27 -0700

    added trailer input, useeffect log

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   const [order, setOrder] = useState([]);
-  const [equipment, setEquipment] = useState('');
+  const [truck, setTruck] = useState('');
+  const [trailer, setTrailer] = useState('');
+
+  useEffect(() => {
+    console.log('order update: ', order);
+  }, [order]);
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(equipment);
-    setOrder((prevOrder) => [...prevOrder, equipment]);
-    setEquipment('');
+    // console.log(truck);
+    setOrder((prevOrder) => [...prevOrder, { truck: truck, trailer: trailer }]);
+    setTruck('');
+    setTrailer('');
   }
 
   return (
     <div>
       <h1>R2R Order</h1>
       <ol>
-        {order.map((item) => (
-          <li key={item}>{item}</li>
+        {order.map((item, index) => (
+          <li key={index}>
+            {item.truck} - {item.trailer}
+          </li>
         ))}
       </ol>
 
       <form onSubmit={handleSubmit}>
-        <h2>Equipment: {equipment}</h2>
+        <h2>
+          Equipment: {truck} {trailer}
+        </h2>
         <label>Equipment number </label>
         <input
           type="text"
-          value={equipment}
-          onChange={(e) => setEquipment(e.target.value)}
+          value={truck}
+          onChange={(e) => setTruck(e.target.value)}
         />
+
+        <br />
+
+        <label>Additional </label>
+        <input
+          type="text"
+          value={trailer}
+          onChange={(e) => setTrailer(e.target.value)}
+        />
+
+        <br />
+
         <button>Add</button>
       </form>
     </div>
